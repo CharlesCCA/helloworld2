effb2bbad0ed88539dd58d2f97f17c3ce96556ca
CharlesCCA <142326442+CharlesCCA@users.noreply.github.com>
2024-02-17 18:01:12 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const header = 'HELLO WORLD!';
+const para1 = 'Hello world is a classic phrase used to show students how to print text using various programming languages.';
+const para2 = 'How much code could a codechuck code if a codechuck could chuck code?'
+
+document.querySelector('.header').innerHTML = header;
+document.querySelector('.para1').innerHTML = para1;
+document.querySelector('.para2').innerHTML = para2;
+document.querySelector('.para3').innerHTML = 'Bruce\'s fashion sense is amazing';
